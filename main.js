b771283aef81724704ba1dbe878f336df8c0462c
vijay <vijaykumar282004@gmail.com>
2025-09-08 16:42:34 +0530

    Initial Push

---
+++ main.js
@@ -0,0 +1,15 @@
+// Mobile menu toggle
+const mobileMenuButton = document.getElementById('mobile-menu-button');
+const mobileMenu = document.getElementById('mobile-menu');
+
+mobileMenuButton.addEventListener('click', () => {
+    mobileMenu.classList.toggle('hidden');
+});
+
+// Close mobile menu when a link is clicked
+const mobileMenuLinks = mobileMenu.getElementsByTagName('a');
+for (let link of mobileMenuLinks) {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.add('hidden');
+    });
+}
